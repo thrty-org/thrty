a93057d81567504fc685763c5cab93d740d2462a
Robin Baum <robinbuschmann@hotmail.de>
2025-03-23 21:48:29 +0100

    Adds logging to tests

--- test/specs/cli/createApiPackage.spec.ts
+++ test/specs/cli/createApiPackage.spec.ts
@@ -11,27 +11,39 @@ const exportName = 'todoApiFactory';
 beforeAll(async () => {
   if (!existsSync(distDir)) mkdirSync(distDir);
 
-  // await createApiPackage({
-  //   pattern: join(__dirname, '..', '__fixtures__', '*Lambda.ts'),
-  //   outDir,
-  //   packageName: 'test-api-generated',
-  //   httpClient: 'axios',
-  //   models: 'zod',
-  //   exportName: 'todoApiFactory',
-  // });
+  try {
+    await createApiPackage({
+      pattern: join(__dirname, '..', '__fixtures__', '*Lambda.ts'),
+      outDir,
+      packageName: 'test-api-generated',
+      httpClient: 'axios',
+      models: 'zod',
+      exportName: 'todoApiFactory',
+    });
+  } catch (e) {
+    console.error(e);
+  }
 });
 
 it('should create package with source files and folders', async () => {
-  // const packagePathParts = await readdir(outDir);
-  // expect(packagePathParts).toEqual(['package.json', 'src']);
-  //
-  // const srcPathParts = await readdir(join(outDir, 'src'));
-  // expect(srcPathParts).toEqual(['index.ts']);
+  try {
+    const packagePathParts = await readdir(outDir);
+    expect(packagePathParts).toEqual(['package.json', 'src']);
+
+    const srcPathParts = await readdir(join(outDir, 'src'));
+    expect(srcPathParts).toEqual(['index.ts']);
+  } catch (e) {
+    console.error(e);
+  }
 });
 
 it('should create api factory', async () => {
-  // const module = require(join(outDir, 'src', 'index.ts'));
-  // const apiFactory = module[exportName];
-  //
-  // expect(apiFactory).toBeDefined();
+  try {
+    const module = require(join(outDir, 'src', 'index.ts'));
+    const apiFactory = module[exportName];
+
+    expect(apiFactory).toBeDefined();
+  } catch (e) {
+    console.error(e);
+  }
 });
